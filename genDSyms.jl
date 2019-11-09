ebe54b521518a66bf6c8ec7804de57ac57dc7e3c
odf <olaf.delgado@gmail.com>
2019-11-09 19:46:12 +1100

    genDSyms: add a print routine

--- genDSyms.jl
+++ genDSyms.jl
@@ -101,7 +101,7 @@ function scan(ds::DSet, w::Vector{Int}, D::Int, limit::Int)
 end
 
 
-function isCanonical(ds)
+function isCanonical(ds::DSet)
     for D in 1 : size(ds)
         if compareRenumberedFrom(ds, D) < 0
             return false
@@ -112,7 +112,7 @@ function isCanonical(ds)
 end
 
 
-function compareRenumberedFrom(ds, D0)
+function compareRenumberedFrom(ds::DSet, D0::Int)
     n2o = zeros(Int, size(ds))
     o2n = zeros(Int, size(ds))
 
@@ -138,6 +138,65 @@ function compareRenumberedFrom(ds, D0)
     end
 
     return 0
+end
+
+
+function Base.show(io::IO, ds::DSet)
+    print(io, "<1.1:", size(ds))
+    if dim(ds) != 2
+        print(io, " ", dim(ds))
+    end
+    print(io, ":")
+
+    for i in 0 : dim(ds)
+        if i > 0
+            print(",")
+        end
+        for D in 1 : size(ds)
+            E = get(ds, i, D)
+            if E == 0 || E >= D
+                if D > 1
+                    print(io, " ")
+                end
+                print(io, E)
+            end
+        end
+    end
+    print(io, ":")
+
+    for i in 0 : dim(ds) - 1
+        if i > 0
+            print(",")
+        end
+        j = i + 1
+        seen = falses(size(ds))
+
+        for D in 1 : size(ds)
+            if !seen[D]
+                if D > 1
+                    print(io, " ")
+                end
+                E, k = D, 0
+                while true
+                    Ei = get(ds, i, E)
+                    E = Ei == 0 ? E : Ei
+                    seen[E] = true
+                    Ej = get(ds, j, E)
+                    E = Ej == 0 ? E : Ej
+                    seen[E] = true
+                    k += 1
+
+                    if E == D
+                        break
+                    end
+                end
+
+                print(io, k * Int(ceil(3 / k)))
+            end
+        end
+    end
+
+    print(io, ">")
 end
 
 
