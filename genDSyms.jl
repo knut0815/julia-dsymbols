b2cbe37f935176fc8f74b2d1a75b3885e29f6f03
odf <olaf.delgado@gmail.com>
2019-11-10 17:56:39 +1100

    genDSyms: compute automorphisms and their actions on the orbits

--- genDSyms.jl
+++ genDSyms.jl
@@ -52,6 +52,68 @@ function orbits(ds::DSet, i::Int)
 end
 
 
+function morphism(ds::DSet, D0::Int)
+    m = zeros(Int, size(ds))
+    m[1] = D0
+    q = [(1, D0)]
+
+    while length(q) > 0
+        D, E = popfirst!(q)
+
+        for i in 0 : dim(ds)
+            Di = get(ds, i, D)
+            Ei = get(ds, i, E)
+
+            if Di > 0 || Ei > 0
+                if m[Di] == 0
+                    m[Di] = Ei
+                    push!(q, (Di, Ei))
+                elseif m[Di] != Ei
+                    return nothing
+                end
+            end
+        end
+    end
+
+    return m
+end
+
+
+function automorphisms(ds::DSet)
+    result::Vector{Vector{Int}} = []
+
+    for D in 1 : size(ds)
+        map = morphism(ds, D)
+        if map != nothing
+            push!(result, map)
+        end
+    end
+
+    return result
+end
+
+
+function onOrbits(map::Vector{Int}, orbs::Vector{Orbit}, ds::DSet)
+    inOrb = zeros(Int, dim(ds), size(ds))
+
+    for i in 1 : length(orbs)
+        for D in orbs[i].elements
+            inOrb[orbs[i].index + 1, D] = i
+        end
+    end
+
+    orbMap = zeros(Int, length(orbs))
+
+    for D in 1 : size(ds)
+        for i in 0 : dim(ds) - 1
+            orbMap[inOrb[i + 1, D]] = inOrb[i + 1, map[D]]
+        end
+    end
+
+    return orbMap
+end
+
+
 function Base.show(io::IO, ds::DSet)
     print(io, "<1.1:", size(ds))
     if dim(ds) != 2
@@ -94,4 +156,15 @@ end
 
 for ds in DSetGenerator(2, parse(Int, ARGS[1]))
     println(ds)
+
+    orbs = vcat(orbits(ds, 0), orbits(ds, 1))
+    elmMaps = automorphisms(ds)
+
+    orbMaps = Set{Vector{Int}}()
+    for m in elmMaps
+        push!(orbMaps, onOrbits(m, orbs, ds))
+    end
+
+    println("# $(orbMaps)")
+    println()
 end
