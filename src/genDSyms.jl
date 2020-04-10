cc4932d4c3ff25f4f8273f98309e517575da6af7
odf <olaf.delgado@gmail.com>
2020-04-10 17:03:36 +1000

    genDSyms: add a command line parameter to filter for pseudo-convexity

--- src/genDSyms.jl
+++ src/genDSyms.jl
@@ -1,17 +1,37 @@
 include("dsetGenerator.jl")
 include("dsymGenerator.jl")
+include("properties.jl")
 
 
-for (count1, dset) in enumerate(DSetGenerator(2, parse(Int64, ARGS[1])))
+filterPseudoConvex = false
+maxSize = 6
+
+for s in ARGS
+    if s == "-c"
+        global filterPseudoConvex
+        filterPseudoConvex = true
+    else
+        n = tryparse(Int64, s)
+        if n != nothing
+            global maxSize
+            maxSize = n
+        end
+    end
+end
+
+
+printIfGood(ds) = (!filterPseudoConvex || isPseudoConvex(ds)) && println(ds)
+
+for (count1, dset) in enumerate(DSetGenerator(2, maxSize))
     orbs = vcat(orbits(dset, 0, 1), orbits(dset, 1, 2))
     vs = map(minV, orbs)
     curv = curvature(dset, orbs, vs)
 
     if curv < 0
-        println(NumberedDelaneySymbol(DelaneySymbol(dset, vs), count1, 1))
+        printIfGood(NumberedDelaneySymbol(DelaneySymbol(dset, vs), count1, 1))
     else
         for (count2, ds) in enumerate(DSymGenerator(dset))
-            println(NumberedDelaneySymbol(ds, count1, count2))
+            printIfGood(NumberedDelaneySymbol(ds, count1, count2))
         end
     end
 end
