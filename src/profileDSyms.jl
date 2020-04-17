d6bfb59512ee34510da3fad22d9be035dd75b02c
odf <olaf.delgado@gmail.com>
2020-04-17 19:15:27 +1000

    profileDSyms: update to cope with faster pseudo-convexity test

--- src/profileDSyms.jl
+++ src/profileDSyms.jl
@@ -25,14 +25,14 @@ end
 
 
 println("Warmup run...")
-gen(6)
+gen(8)
 
 Profile.init(n = 10000000)
 Profile.clear()
 
 println("Profile run...")
-@profile gen(10)
+@profile gen(12)
 
 println("Profiling result:")
-Profile.print(format=:tree, combine=true, mincount=1000)
+Profile.print(format=:tree, combine=true, mincount=300)
 #Profile.print(format=:flat, combine=true, sortedby=:count)
