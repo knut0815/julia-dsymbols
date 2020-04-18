464f13baa7a7f3bc99e5de52c70c91cff55d24dd
odf <olaf.delgado@gmail.com>
2020-04-18 18:56:06 +1000

    profileDSyms: simplify

--- src/profileDSyms.jl
+++ src/profileDSyms.jl
@@ -7,18 +7,9 @@ include("properties.jl")
 
 function gen(n)
     for (count1, dset) in enumerate(DSetGenerator(2, n))
-        orbs = vcat(orbits(dset, 0, 1), orbits(dset, 1, 2))
-        vs = map(minV, orbs)
-        curv = curvature(dset, orbs, vs)
-
-        if curv < 0
-            isPseudoConvex(DelaneySymbol(dset, vs))
-            #println(NumberedDelaneySymbol(DelaneySymbol(dset, vs), count1, 1))
-        else
-            for (count2, ds) in enumerate(DSymGenerator(dset))
-                isPseudoConvex(ds)
-                #println(NumberedDelaneySymbol(ds, count1, count2))
-            end
+        for (count2, ds) in enumerate(DSymGenerator(dset))
+            isPseudoConvex(ds)
+            #println(NumberedDelaneySymbol(ds, count1, count2))
         end
     end
 end
